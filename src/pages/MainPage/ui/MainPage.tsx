b2614a1b2d46c0e0eb0d86345e0c578f7a9c7937
Vadim Krasouski <vadzim.krasouski@gmail.com>
2024-08-18 18:00:06 +0200

    import order

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './MainPage.module.scss';
 import { BugButton } from 'app/providers/ErrorBoundary/ui/BugButton';
+import cls from './MainPage.module.scss';
 
 interface MainPageProps {
     className?: string;
